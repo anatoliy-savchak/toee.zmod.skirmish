7d5298d6dfa0903b96928fee88b6df8779a97253
anatoliy_savchak <anatoliy.savchak@gmail.com>
2021-12-18 15:37:48 +0200

    Shambling Mound

--- src/zmod_skirmish_core/scr/py07710_skirmish_harbinger_monsters.py
+++ src/zmod_skirmish_core/scr/py07710_skirmish_harbinger_monsters.py
@@ -47,6 +47,7 @@ def get_character_classes():
 		, CtrlLEHalfOrcMonk
 		, CtrlLEDireBoar
 		, CtrlLELizardfolk
+		, CtrlLEShamblingMound
 		, CtrlLEHumanBlackguardAsPC
 	]
 	return result
@@ -85,6 +86,7 @@ def get_enemy_classes():
 		, CtrlLEHalfOrcMonk
 		, CtrlLEDireBoar
 		, CtrlLELizardfolk
+		, CtrlLEShamblingMound
 		, CtrlLEHumanBlackguard
 	]
 	return result
@@ -1996,6 +1998,54 @@ class CtrlLELizardfolk(CtrlSkirmisherLE):
 		utils_npc.npc_generate_hp_avg_first(npc, 0)
 		npc.item_wield_best_all()
 		return
+
+class CtrlLEShamblingMound(CtrlSkirmisherLE):
+	@classmethod
+	def get_proto_id(cls): return 14889
+
+	@classmethod
+	def get_price(cls): return 30
+
+	@classmethod
+	def get_title(cls): return "Shambling Mound"
+
+	@classmethod
+	def get_alignment_groups(cls): return [cls.get_alignment_group(), toee.ALIGNMENT_LAWFUL_GOOD, toee.ALIGNMENT_CHAOTIC_GOOD, toee.ALIGNMENT_CHAOTIC_EVIL]
+
+	def after_created(self, npc):
+		assert isinstance(npc, toee.PyObjHandle)
+
+		#utils_npc.npc_hitdice_set(npc, 2, 8, 0)
+		#utils_npc.npc_abilities_set(npc, [13, 10, 13, 9, 10, 10])
+
+		#npc.obj_set_int(toee.obj_f_critter_portrait, 3520) 
+		#npc.obj_set_int(toee.obj_f_critter_gender, toee.gender_male)
+		#npc.obj_set_int(toee.obj_f_pc_voice_idx, const_toee.pcv_lawful)
+		npc.obj_set_int(toee.obj_f_critter_alignment, toee.ALIGNMENT_NEUTRAL)
+		#npc.obj_set_int(toee.obj_f_npc_ac_bonus, 5) # natural ac
+		#npc.obj_set_int(toee.obj_f_npc_save_fortitude_bonus, 5)
+		#npc.obj_set_int(toee.obj_f_npc_save_reflexes_bonus, 5)
+		#npc.obj_set_int(toee.obj_f_npc_save_willpower_bonus, 5)
+
+		npc.obj_set_idx_int(toee.obj_f_attack_types_idx, 0, const_toee.nwt_slam)
+		npc.obj_set_idx_int(toee.obj_f_attack_bonus_idx, 0, 6) # natural bab
+		npc.obj_set_idx_int(toee.obj_f_critter_attacks_idx, 0, 1) # 1x
+		npc.obj_set_idx_int(toee.obj_f_critter_damage_idx, 0, toee.dice_new("2d6").packed)
+
+		npc.condition_add_with_args("Base_Movement", 0, 66) # should be 20 ft, factor: 0.66 = 20/30
+		#npc.condition_add_with_args("Monster Bonus Damage", toee.D20DT_FIRE, toee.dice_new("1d6").packed)
+
+		npc.feat_add(toee.feat_weapon_focus_unarmed_strike_medium_sized_being, 0)
+		npc.feat_add(toee.feat_alertness, 1)
+		self.setup_name(npc, self.get_title())
+
+		#self._hide_loot(utils_item.item_create_in_inventory(const_proto_armor.PROTO_SHIELD_LARGE_WOODEN_2, npc))
+		#self._hide_loot(utils_item.item_create_in_inventory(4008, npc)) # Lizardman Club
+
+		utils_npc.npc_generate_hp_avg_first(npc, 0)
+		npc.item_wield_best_all()
+		return
+
 
 class CtrlLEHumanBlackguard(CtrlSkirmisherLE):
 	@classmethod
